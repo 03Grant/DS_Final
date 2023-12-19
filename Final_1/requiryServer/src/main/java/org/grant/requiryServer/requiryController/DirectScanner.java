d54616f462506efd6a841cf3803159ef14a859ea
LuminousWanderer <1664524275@qq.com>
2023-12-19 18:15:29 +0800

    xml文件索引建立

--- Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/DirectScanner.java
+++ Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/DirectScanner.java
@@ -1,5 +1,56 @@
 package org.grant.requiryServer.requiryController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class DirectScanner {
-    //todo to be done
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            // 获取用户输入的XML文件路径
+            System.out.print("请输入XML文件路径：");
+            String xmlFilePath = scanner.nextLine();
+
+            long startTime = System.currentTimeMillis();
+
+            // 使用 ProcessBuilder 运行 Shell 命令
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c",
+                    "grep -E '<author>|<www mdate=' " + xmlFilePath + " | sed 's/.*<author>\\(.*\\)<\\/author>.*/\\1/' > authors.txt && " +
+                            "grep '<www mdate=' " + xmlFilePath + " | awk -F'-' '{print $1}' > years.txt && " +
+                            "paste authors.txt years.txt | sort | uniq -c");
+
+            Process process = processBuilder.start();
+
+            // 等待命令执行完成
+            int exitCode = process.waitFor();
+
+            // 打印执行结果
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+
+            // 打印执行时间
+            System.out.println("Command executed in " + elapsedTime + " milliseconds");
+
+            // 检查命令是否成功执行
+            if (exitCode == 0) {
+                System.out.println("Command executed successfully");
+            } else {
+                System.out.println("Command failed with exit code: " + exitCode);
+            }
+
+            // 关闭Scanner
+            scanner.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/HashScanner.java
+++ Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/HashScanner.java
@@ -1,5 +1,60 @@
 package org.grant.requiryServer.requiryController;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.Scanner;
+
 public class HashScanner {
-    //todo to be done
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter author name: ");
+        String authorToQuery = scanner.nextLine();
+
+        System.out.print("Enter year: ");
+        int yearToQuery = scanner.nextInt();
+
+        int paperCount = queryPaperCount(authorToQuery, yearToQuery);
+
+        System.out.println(authorToQuery + " published " + paperCount + " papers in " + yearToQuery + ".");
+    }
+
+    private static int queryPaperCount(String author, int year) {
+        try {
+            File xmlFile = new File("output.xml");
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList yearList = doc.getElementsByTagName("Year");
+            for (int i = 0; i < yearList.getLength(); i++) {
+                Element yearElement = (Element) yearList.item(i);
+                int currentYear = Integer.parseInt(yearElement.getAttribute("value"));
+
+                if (currentYear == year) {
+                    NodeList authorList = yearElement.getElementsByTagName("Author");
+                    for (int j = 0; j < authorList.getLength(); j++) {
+                        Element authorElement = (Element) authorList.item(j);
+                        String currentAuthor = authorElement.getAttribute("name");
+
+                        if (currentAuthor.equals(author)) {
+                            return Integer.parseInt(authorElement.getAttribute("frequency"));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
 }
