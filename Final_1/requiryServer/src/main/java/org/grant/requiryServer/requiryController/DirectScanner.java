0da38b15c244102365df5569bd2e0e884811173a
LuminousWanderer <1664524275@qq.com>
2023-12-26 01:37:32 +0800

    命令行扫描

--- Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/DirectScanner.java
+++ Final_1/requiryServer/src/main/java/org/grant/requiryServer/requiryController/DirectScanner.java
@@ -1,65 +1,161 @@
 package org.grant.requiryServer.requiryController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class DirectScanner {
 
     @GetMapping("/final1/directscan")
-    public void DirectScan(@RequestParam String author, @RequestParam int beginyear, @RequestParam int endyear) {
+    public ResponseEntity<String> DirectScan(@RequestParam String author, @RequestParam int beginyear, @RequestParam int endyear) {
+
+        String directoryPath = "/app/fileWare/";
+        File directory = new File(directoryPath);
+
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+
+            if (files != null && files.length > 0) {
+                // 创建字符数组来存储文件名
+                String[] fileNames = new String[files.length];
+
+                // 提取文件名并放入字符数组中
+                for (int i = 0; i < files.length; i++) {
+                    fileNames[i] = files[i].getName();
+                }
+                List<Map<String, Object>> dataList = new ArrayList<>();
+                try {
+                    for (String fileName : fileNames) {
+                        String authorToQuery = author;
+                        List<Integer> years = new ArrayList<>();
+                        for (int year = beginyear; year <= endyear; year++) {
+                            years.add(year);
+                        }
+                        int paperCount = queryPaperCount(directoryPath, fileName, authorToQuery, years);
+                        Map<String, Object> num = parseXmlPath(fileName);
+                        String blockNumber = (String) num.get("blocknum");
+                        String replicaNumber = (String) num.get("repnum");
+                        Map<String, Object> block = new HashMap<>();
+                        block.put("blocknum", blockNumber);
+                        block.put("repnum", replicaNumber);
+                        block.put("num", paperCount);
+                        dataList.add(block);
+                    }
+                    return new ResponseEntity<>(dataList.toString(), HttpStatus.OK);
+                } catch (Exception e) {
+                    return new ResponseEntity<>("查询失败", HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                return new ResponseEntity<>("目录为空", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<>("目录无效", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static Map<String, Object> parseXmlPath(String xmlName) {
+        // 定义匹配blocknum和副本号的正则表达式
+        Pattern pattern = Pattern.compile("(\\d+)_(\\d+)\\.xml");
+
+        // 创建 Matcher 对象
+        Matcher matcher = pattern.matcher(xmlName);
+
+        Map<String, Object> resultJson = new HashMap<>();
+        // 查找匹配
+        if (matcher.find()) {
+            // 提取blocknum和副本号
+            String blockNumber = matcher.group(1);
+            String replicaNumber = matcher.group(2);
+
+            // 将blocknum和副本号放入 resultJson
+            resultJson.put("blocknum", blockNumber);
+            resultJson.put("repnum", replicaNumber);
+        }
+
+        return resultJson;
+    }
+
+    private static int queryPaperCount(String directoryPath, String xmlName, String author, List<Integer> years) {
         try {
-            Scanner scanner = new Scanner(System.in);
+            // 构建 AWK 命令
+            String awkCommand = buildAwkCommand(directoryPath, xmlName, author, years);
 
-            // 获取用户输入的XML文件路径
-            //System.out.print("请输入XML文件路径：");
-           // String xmlFilePath = scanner.nextLine();
-            String xmlFilePath = "app/fileWare";
-            long startTime = System.currentTimeMillis();
-
-            // 使用 ProcessBuilder 运行 Shell 命令
-            ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c",
-                    "grep -E '<author>|<www mdate=' " + xmlFilePath + " | sed 's/.*<author>\\(.*\\)<\\/author>.*/\\1/' > authors.txt && " +
-                            "grep '<www mdate=' " + xmlFilePath + " | awk -F'-' '{print $1}' > years.txt && " +
-                            "paste authors.txt years.txt | sort | uniq -c");
-
+            // 使用 ProcessBuilder 运行 AWK 命令
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", awkCommand);
             Process process = processBuilder.start();
+
+            // 获取命令的输出流
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+
+            // 读取命令输出并拼接到 StringBuilder 中
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
 
             // 等待命令执行完成
             int exitCode = process.waitFor();
 
             // 打印执行结果
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+            System.out.println("Command executed successfully");
+            System.out.println("Output: " + output);
 
-            long endTime = System.currentTimeMillis();
-            long elapsedTime = endTime - startTime;
+            // 解析 AWK 命令的输出结果，提取论文数
+            int paperCount = parseOutput(output.toString());
 
+            return paperCount;
 
-            //todo 查询结束后返回一个response之类的
-            // 打印执行时间
-            System.out.println("Command executed in " + elapsedTime + " milliseconds");
-
-            // 检查命令是否成功执行
-            if (exitCode == 0) {
-                System.out.println("Command executed successfully");
-            } else {
-                System.out.println("Command failed with exit code: " + exitCode);
-            }
-
-            // 关闭Scanner
-            scanner.close();
-
-        } catch (Exception e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            return -1; // 返回一个错误码或异常值
         }
     }
+
+    private static String buildAwkCommand(String directoryPath, String xmlName, String author, List<Integer> years) {
+        StringBuilder commandBuilder = new StringBuilder();
+
+        // 构建 AWK 命令
+        commandBuilder.append("awk -F'[<>]' '/<author>").append(author).append("<\\/author>/ { author_found=1 } ");
+        commandBuilder.append("/<year>([0-9]+)<\\/year>/ { if (author_found && ($3 >= ");
+
+        // 添加年份范围
+        for (int year : years) {
+            commandBuilder.append(year).append(" || $3 == ").append(year).append(" || ");
+        }
+        commandBuilder.delete(commandBuilder.length() - 4, commandBuilder.length());
+
+        commandBuilder.append(")) { count++ } author_found=0 } END { print \"").append(author).append(" 论文数:\", count }' ");
+        commandBuilder.append(directoryPath).append("/").append(xmlName);
+
+        return commandBuilder.toString();
+    }
+
+    private static int parseOutput(String output) {
+        // 解析 AWK 命令的输出结果，提取论文数
+        String[] lines = output.split("\n");
+        if (lines.length > 0) {
+            String lastLine = lines[lines.length - 1];
+            try {
+                return Integer.parseInt(lastLine.trim());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1; // 返回一个错误码或异常值
+    }
+
 }
